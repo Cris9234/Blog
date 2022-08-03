e8aacaa27175ad69cecf0b514fb5fffafc3adcc2
Cristiano Angeli <cristiano.a992@gmail.com>
2022-08-03 11:49:50 +0200

    add Post class create superuser make admin dashboard more efficient

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    # display the properties mentioned in the tuple
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # add the filtering by status
+    list_filter = ('status',)
+    # add the search bar which will search the data considering the attributes of this field
+    search_fields = ['title', 'content']
+    # to create automatically the slug based on the title
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
